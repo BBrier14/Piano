5feda7ee893bd084bf7ba24462c51d0ac016a1e3
Benjamin Brier <ben.brier14@gmail.com>
2020-05-08 19:20:48 -0400

    added event listener

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ const blackKeys = document.querySelectorAll('.key.black');
 keys.forEach((key) => {
 	key.addEventListener('click', () => playNote(key));
 });
+
+document.addEventListener('keydown', (e) => {
+	if (e.repeat) return;
+	const key = e.key;
+	const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+	const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+	if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
+	if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
+});
